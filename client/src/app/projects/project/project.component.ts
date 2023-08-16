ba79a0b5739cd5fd0e3a23fc2c74428e85e3d2d1
JamesCharnley <james.anthony.charnley@gmail.com>
2023-08-17 11:51:00 +1200

    Updated angular to check if external url is github or itch and change button text to match

--- client/src/app/projects/project/project.component.ts
+++ client/src/app/projects/project/project.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Project } from 'src/app/_models/project';
 import { ProjectsService } from 'src/app/_services/projects.service';
 import {DomSanitizer} from '@angular/platform-browser';
+import { getCurrencySymbol } from '@angular/common';
 
 @Component({
   selector: 'app-project',
@@ -12,11 +13,13 @@ import {DomSanitizer} from '@angular/platform-browser';
 export class ProjectComponent implements OnInit {
 
   project: Project | undefined;
+  urlName: string | undefined;
   
   constructor(private projectServices: ProjectsService, private route: ActivatedRoute, private sanitizerService: DomSanitizer) { }
 
   ngOnInit(): void {
     this.loadProject();
+    //this.urlName = this.getExternalUrlName();
   }
 
   loadProject() {
@@ -26,10 +29,54 @@ export class ProjectComponent implements OnInit {
       return;
     } 
     this.projectServices.getProject(projectId).subscribe({
-      next: project => this.project = project
+      next: project => this.project = project,
+      error: error => console.log(error),
+      complete: () => this.urlName = this.getExternalUrlName()
     })
   }
 
+  getExternalUrlName()
+  {
+    if(this.project)
+    {
+      if(this.project.externalUrl)
+      {
+        let substr= 'itch';
+
+        if(this.containsSubStr(this.project.externalUrl.toString(), substr))
+        {
+          return 'Itch.io';
+        }
+
+        substr = 'github';
+
+        if(this.containsSubStr(this.project.externalUrl.toString(), substr))
+        {
+          return 'Github';
+        }
+        
+        return 'More';
+      }
+    }
+    else
+    {
+      console.log("project is null");
+    }
+    console.log("getExternalUrl returned undefined");
+    return undefined;
+  }
+  containsSubStr(str: string, substr: string)
+  {
+    const result = str.includes(substr);
+    if(result)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
   videoURL(){
     if(this.project)
     return this.sanitizerService.bypassSecurityTrustResourceUrl(this.project.videoUrl);
